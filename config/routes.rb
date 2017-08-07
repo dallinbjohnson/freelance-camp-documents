d926b4728a5db9ea749457757edd5251adb216bd
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-08-07 10:12:20 -0600

    added a root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :freelance_documents
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "freelance_documents#index"
 end
